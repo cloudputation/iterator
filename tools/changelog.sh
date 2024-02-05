c10ad74f8c1d08773d4ed1e2a7ecb295a5ad8ada
FranksRobins <francis.f.robinson@gmail.com>
2024-02-04 23:00:56 -0500

    changelog.sh update

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -4,14 +4,6 @@
 # It include only the last changes for the current release.
 
 CHANGELOG_CONTENT=$(awk '/^## / {n++} n == 2 {exit} {if(n > 0) print}' CHANGELOG.md)
-echo "Including change logs for current release"
-echo "${CHANGELOG_CONTENT}"
-echo
-echo
-echo
-echo
-echo
-echo
 CHANGELOG_CONTENT="${CHANGELOG_CONTENT//'%'/'%25'}"
 CHANGELOG_CONTENT="${CHANGELOG_CONTENT//$'\n'/'%0A'}"
 CHANGELOG_CONTENT="${CHANGELOG_CONTENT//$'\r'/'%0D'}"
